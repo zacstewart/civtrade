5c534aa7a7dc42838c94ef199f89a0dfc562deee
Zac Stewart <zgstewart@gmail.com>
2012-12-23 13:12:36 -0500

    Disable Minecraft verification

--- app/models/signup.rb
+++ app/models/signup.rb
@@ -7,14 +7,11 @@ class Signup
   attribute :username
   attribute :password
   attribute :password_confirmation
-  attribute :minecraft_login
-  attribute :minecraft_password
 
   attr_reader :user
 
-  validates :username, :password, :minecraft_login, :minecraft_password, presence: true
+  validates :username, :password, presence: true
   validates :password, confirmation: true
-  validate :is_authentic_minecraft_user
   validate :is_unique_username
 
   def persisted?
@@ -33,13 +30,6 @@ class Signup
   def is_unique_username
     if User.where(username: username).exists?
       errors.add(:username, 'is already signed up')
-    end
-  end
-
-  def is_authentic_minecraft_user
-    verifier = MinecraftAccountVerifier.new(minecraft_login, username, minecraft_password)
-    unless verifier.authentic?
-      errors.add(:minecraft_login, verifier.error)
     end
   end
 
