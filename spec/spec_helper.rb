9b1966193707a35d72c7b6959535e1965a22b1c5
Zac Stewart <zgstewart@gmail.com>
2013-03-09 13:14:29 -0500

    Use capybara-webkit for js tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,8 @@ require 'rspec/rails'
 require 'rspec/autorun'
 require 'blueprints'
 
+Capybara.javascript_driver = :webkit
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
