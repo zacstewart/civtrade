72be101330fd3d382d3b90147c12ac92d50d5532
Zac Stewart <zgstewart@gmail.com>
2013-06-08 14:49:33 -0400

    Use validation instead of stupid save hack

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,5 +1,6 @@
 class Shop < ActiveRecord::Base
   CIVCRAFT_SERVER_PATTERN = /mc\.civcraft\.vg:25565|untamedears.com:25565/
+  CIVCRAFT_SERVER_PATTERN = /(127.0.0.1|localhost):25565/
 
   include PgSearch
 
@@ -10,6 +11,7 @@ class Shop < ActiveRecord::Base
   validates :buy_amount, :buy_price, :buy_currency, presence: true, unless: :selling?
   validates :buy_currency, :sell_currency, inclusion: {in: CURRENCIES}, allow_blank: true
   validates :world, inclusion: {in: WORLDS}
+  validate :server_is_test_or_civcraft
 
   belongs_to :item
   has_many :reports
@@ -34,8 +36,10 @@ class Shop < ActiveRecord::Base
     sell_amount.present? && sell_price.present? && sell_currency.present?
   end
 
-  def save
-    return false unless server.blank? || server =~ CIVCRAFT_SERVER_PATTERN
-    super
+  private
+  def server_is_test_or_civcraft
+    unless server.blank?  || server =~ CIVCRAFT_SERVER_PATTERN || server =~ LOCALHOST
+      errors.add(:server, "You can only list Civcraft shops")
+    end
   end
 end
