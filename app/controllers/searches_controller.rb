6a61272f8e947736951f77c4b0a42c2c66139b15
Zac Stewart <zgstewart@gmail.com>
2016-09-12 15:14:23 -0400

    Make world query param optional

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,7 +1,7 @@
 class SearchesController < ApplicationController
   def show
     @shops = IndexedShop.scoped
-    @shops = @shops.in_world(params[:world_uuid])
+    @shops = @shops.in_world(params[:world_uuid]) if params[:world_uuid].present?
     @shops = @shops.search(params[:search]) if params[:search].present?
 
     if params[:near_x].present? || params[:near_z].present?
